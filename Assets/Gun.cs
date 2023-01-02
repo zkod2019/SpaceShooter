823917748e8ec7bc53f780e3e41a7c93bf04983e
Zaya Kod <zkod777@gmail.com>
2023-01-02 16:54:47 +0400

    first commit

---
+++ Assets/Gun.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://www.youtube.com/watch?v=LNLVOjbrQj4 6:57
+
+public class Gun : MonoBehaviour
+{
+    public Transform BulletPlacement;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+
+    public float moveSpeed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            var bullet = Instantiate(bulletPrefab, BulletPlacement.position, BulletPlacement.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = BulletPlacement.forward * bulletSpeed;
+        }
+
+        if(Input.GetMouseButton(0)){
+          transform.eulerAngles += moveSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), z:0);
+        }        
+
+    }
+
+   
+}
